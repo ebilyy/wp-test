71755ea6f918070f348892954b99011bd4e479f1
Eugene Bilyy <gene.bilyy@gmail.com>
2017-09-04 21:38:46 +0300

    some changes

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # wp-test
-wp-coding
+1. done
+2. not done
+3. done
+4. done
+5. Don't know how to do this without plugin 
---
+++ wp-content/themes/twentyseventeen/template-parts/footer/site-contacts.php
@@ -0,0 +1,15 @@
+
+<div class="site-contacts">
+
+<div class="btn">
+    <span class="mob-hide">Call Us: </span>
+    <a href="tel:380987654321">
+        <span class="mob-hide">380987654321</span> 
+        <span class="mob-show">Call Us:</span> 
+    </a>
+</div>    
+<div>
+<a class="btn" href="mailto:testdomain@mail.to"><span class="mob-hide">testdomain@mail.to</span><span class="mob-show">Email Us</span></a>
+</div>
+<button class="btn">Contact Us</button>
+</div>
